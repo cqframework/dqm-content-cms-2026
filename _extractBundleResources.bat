40f95fb70af7f497c641b0d83016439a3eefa4cd
Bryn Rhodes <bryn@alphora.com>
2026-05-17 01:30:59 -0600

    Added CQL representation of NCT Clinical Trial Eligibility Criteria

--- _extractBundleResources.bat
+++ _extractBundleResources.bat
@@ -2,7 +2,7 @@
 SET tooling_jar=tooling-cli-3.9.1.jar
 SET input_cache_path=%~dp0input-cache
 IF -%1-==-- (
-	SET mat_bundle=input\tests\measure\CMS2FHIRPCSDepressionScreenAndFollowUp
+	SET mat_bundle=input\tests\group\NCT01980823EligibilityCriteria
 ) ELSE (
 	SET mat_bundle=%1
 )
